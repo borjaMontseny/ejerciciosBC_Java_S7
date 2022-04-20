34324ae96d21d936ee939dc8e98ccc2408fbaafb
borjaMontseny <borja.montseny@gmail.com>
2022-04-20 12:48:00 +0200

    Subida del ejercicio1 TA10 blank

---
+++ src/ejercicio1/ejercicio1App.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class ejercicio1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
