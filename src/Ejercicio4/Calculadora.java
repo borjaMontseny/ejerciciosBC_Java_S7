8b31ae864e1e0258b2bfa785bc29cb6c1b16d17a
borjaMontseny <borja.montseny@gmail.com>
2022-04-26 21:54:42 +0200

    Subida del Ej4

---
+++ src/Ejercicio4/Calculadora.java
@@ -0,0 +1,26 @@
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	// Atributos
+	private char operador = 0;
+	private double valor1 = 0;
+	private double valor2 = 0;
+	
+	
+	// Constructores
+	
+	public Calculadora() {
+		
+	}
+	
+	// Métodos
+	
+	public void seleccionarOperador() {
+		System.out.print("Selecciona el operador ");
+	}
+	
+	
+}
--- src/Ejercicio4/Ejerciciio4App.java
+++ src/Ejercicio4/Ejerciciio4App.java
@@ -4,7 +4,9 @@ public class Ejerciciio4App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
